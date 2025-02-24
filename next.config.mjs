20bb82c335d7ccb147264c3433af32b8af79d650
AmirHosienShokri <amirhosien.shokrii@gmail.com>
2025-02-24 19:13:34 +0330

    ii

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
+  images: { 
+    domains: ['next-coffee-shokri.vercel.app'],
     remotePatterns: [
       {
         hostname: "ntdohxdqsywenwtnhkjj.supabase.co",
